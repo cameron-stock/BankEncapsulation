6503a70273abea8d2b8a7d35ba4dd746a7ef8a43
Cameron-Stock <clstock33@gmail.com>
2020-11-08 19:55:01 -0600

    Added phone number configuration class

---
+++ BankEncapsulation/PersonalInformation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace BankEncapsulation
+{
+    public class PersonalInformation
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+
+        private string phoneNumber;
+
+        public string NumberFormatted
+        {
+            get
+            {
+                return phoneNumber;
+
+            }
+            set
+            {
+                phoneNumber = Regex.Replace($"{value}", @"(\d{3})(\d{3})(\d{4})", "$1-$2-$3");
+            }
+
+        }
+       
+       
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -6,6 +6,9 @@ namespace BankEncapsulation
     {
         static void Main(string[] args)
         {
+
+            //Bank account
+
             var account = new BankAccount();
 
             Console.WriteLine("How much would you like to deposit?");
@@ -14,6 +17,27 @@ namespace BankEncapsulation
             account.Deposit(amountToDeposit);
 
             Console.WriteLine($"Thank you! Your balance is now {account.GetBalance()}");
+
+            //Phone number
+
+            var accountInfo = new PersonalInformation();
+            string number;
+            long result;
+
+            do
+            {
+                Console.WriteLine("What is your phone number starting with the area code?");
+                Console.WriteLine("With the following format: 8171231234");
+                number = Console.ReadLine();
+
+            } while (number.Length != 10 || !long.TryParse(number, out result));
+
+            accountInfo.NumberFormatted = number;
+            Console.WriteLine(accountInfo.NumberFormatted);
+
+
+            // Console.WriteLine($" {accountInfo.NumberFormatted}");
+
 
         }
     }
